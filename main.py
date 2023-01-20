f82419f67687c5c6a6d40d44eec206aac1a0d404
casparcrop94 <casparcrop@gmail.com>
2023-01-20 12:08:03 +0100

    Worked on key(board) events

--- main.py
+++ main.py
@@ -7,9 +7,11 @@ pygame.display.set_caption('Runner')
 clock = pygame.time.Clock()
 test_font = pygame.font.Font('font/Pixeltype.ttf', 50)
 
-sky_surface = pygame.image.load('graphics/Sky.png').convert()
-ground_surface = pygame.image.load('graphics/Ground.png').convert()
-text_surface = test_font.render('My Game', False, 'Black')
+sky_surf = pygame.image.load('graphics/Sky.png').convert()
+ground_surf = pygame.image.load('graphics/Ground.png').convert()
+
+score_surf = test_font.render('My Game', False, (64,64,64))
+score_rect = score_surf.get_rect(center = (400, 50))
 
 snail_surf = pygame.image.load('graphics/snail/snail1.png').convert_alpha()
 snail_rect = snail_surf.get_rect(bottomright = (750,300))
@@ -28,10 +30,34 @@ while True:
 
         # if event.type == pygame.MOUSEBUTTONUP:
         #     print('Mouse Up!')
+        if event.type == pygame.KEYDOWN:
+            print('key down')
+            if event.key == pygame.K_SPACE:
+                print('Jump!')
+        if event.type == pygame.KEYUP:
+            print('key up')
 
-    screen.blit(sky_surface,(0,0))
-    screen.blit(ground_surface, (0,300))
-    screen.blit(text_surface, (300,50))
+
+    screen.blit(sky_surf,(0,0))
+    screen.blit(ground_surf, (0,300))
+
+    pygame.draw.rect(screen, '#c0e8ec', score_rect)
+    pygame.draw.rect(screen, '#c0e8ec', score_rect, 10)
+    screen.blit(score_surf, score_rect)
+
+    # keys = pygame.key.get_pressed()
+    
+    # if keys[pygame.K_SPACE]:
+    #     print('Jump!')
+
+
+
+    # line_tlbr = pygame.draw.line(screen, 'Red', (0,0),(800,400),3)
+    # line_bltr = pygame.draw.line(screen, 'Red', (0,400),(800,0),3)
+    # screen.blit(screen, line_tlbr)
+    # screen.blit(screen, line_bltr)
+    # pygame.draw.ellipse(screen, 'Brown', pygame.Rect(50,200,100,100))
+
 
     screen.blit(snail_surf, snail_rect)
     snail_rect.x -= 4
